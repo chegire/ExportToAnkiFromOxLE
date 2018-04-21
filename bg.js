54ef39f4f0d58429ce6d9dce18d5b6b457a2232f
Эдуард Шабанов <chegire35@gmail.com>
2018-04-21 13:04:15 +0500

    Добавлена возможность вызвать окно расширения при нажатии на кнопку на панели браузера

--- bg.js
+++ bg.js
@@ -1,25 +1,33 @@
 chrome.runtime.onInstalled.addListener(function () {
-  // Create one test item for each context type.
-  var context = "selection"
-  var title = "Find in oxle";
-  var id = chrome.contextMenus.create({
-    "id": "sendToOxle",
-    "title": title,
-    "contexts": [context],
-  });
+    // Create one test item for each context type.
+    var context = "selection"
+    var title = "Find in oxle";
+    var id = chrome.contextMenus.create({
+        "id": "sendToOxle",
+        "title": title,
+        "contexts": [context],
+    });
 });
+
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  debugger;
-  if (info.menuItemId != 'sendToOxle')
-    return;
-  settings = chrome.storage.sync.get({
-    definitionsCheckbox: 0,
-    examplesCheckbox: 0,
-    windowWidth: 1024,
-    windowHeight: 768
-  }, function (items) {
-    window.open("popup.html?" + info.selectionText, "extension_popup", "width=" + items.windowWidth + ",height=" + items.windowHeight + ",scrollbars=yes,resizable=no");
-  });
-})
+    if (info.menuItemId != 'sendToOxle')
+        return;
 
+    openPopup(info.selectionText);
+});
 
+function openPopup(query) {
+    chrome.storage.sync.get({
+        definitionsCheckbox: 0,
+        examplesCheckbox: 0,
+        windowWidth: 1024,
+        windowHeight: 768
+    }, function (items) {
+        window.open(
+            "popup.html?" + query, "extension_popup", "width=" + items.windowWidth +
+            ",height=" + items.windowHeight + ",scrollbars=yes,resizable=no"
+        );
+    });
+}
+
+chrome.browserAction.onClicked.addListener(() => openPopup(''));
